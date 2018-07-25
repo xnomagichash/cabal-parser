84a43ea7655686f46d744f6fad6e749efe19d707
Brendan Kohler <brendan@sentenai.com>
2018-07-25 09:07:06 -0400

    Add root path to API.

--- src/API.hs
+++ src/API.hs
@@ -1,18 +1,21 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module API
   ( API
   , server
   ) where
 
+import Data.Text (Text)
 import Dependencies
 import Dependency
 import Distribution.Types.GenericPackageDescription (GenericPackageDescription)
 import Distribution.Types.GenericPackageDescription.MimeUnrender ()
-import Servant ((:>), JSON, PlainText, Post, ReqBody, Server)
+import Servant
 
 type API = "parse" :> ReqBody '[PlainText] GenericPackageDescription :> Post '[JSON] [Dependency]
+         :<|> Get '[PlainText] Text
 
 server :: Server API
-server = return . dependencies
+server = (return . dependencies) :<|> return ""
